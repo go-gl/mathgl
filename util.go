e5546a7912dbf18c39dc9d3d5feec18d15f203cd
Jsor <Jragonmiris@gmail.com>
2013-02-20 11:01:56 -0700

    Added explicit comparison method for floats

--- util.go
+++ util.go
@@ -5,12 +5,30 @@ import "math"
 // Epsilon is some tiny value that determines how precisely equal we want out floats to be
 const epsilon float64 = 1e-15
 
-// FloatEquil is a safe utility function to compare floats.
+// FloatEqual is a safe utility function to compare floats.
 // It's Taken from http://floating-point-gui.de/errors/comparison/
 //
 // It is slightly altered to not call Abs when not needed.
 // Keep in mind that it expects float32s to be converted to float64s before being passed in, because they have to be converted for Abs anyway
 func FloatEqual(a, b float64) bool {
+	if a == b { // Handles the case of inf or shortcuts the loop when no significant error has accumulated
+		return true
+	} else if a*b == 0 { // If a or b are 0
+		return math.Abs(a-b) < epsilon*epsilon
+	}
+
+	// Else compare difference
+	return math.Abs(a-b)/(math.Abs(a)+math.Abs(b)) < epsilon
+}
+
+// FloatEqualThreshold is a utility function to compare floats.
+// It's Taken from http://floating-point-gui.de/errors/comparison/
+//
+// It is slightly altered to not call Abs when not needed.
+// Keep in mind that it expects float32s to be converted to float64s before being passed in, because they have to be converted for Abs anyway
+//
+// This differs from FloatEqual in that it lets you pass in your comparison threshold, so that you can adjust the comparison value to your specific needs
+func FloatEqualThreshold(a, b, epsilon float64) bool {
 	if a == b { // Handles the case of inf or shortcuts the loop when no significant error has accumulated
 		return true
 	} else if a*b == 0 { // If a or b are 0
