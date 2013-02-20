f92bfd4f820ac0fb28182f0c6c21fd2aaa44be53
Jsor <Jragonmiris@gmail.com>
2013-02-19 23:27:36 -0700

    Formatting

--- transform.go
+++ transform.go
@@ -4,7 +4,8 @@ import "math"
 
 func Rotate2D(angle float64) Matrix {
 	sin, cos := math.Sin(angle), math.Cos(angle)
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, -sin, sin, cos}, FLOAT64), FLOAT64, 2, 2)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, -sin,
+		sin, cos}, FLOAT64), FLOAT64, 2, 2)
 }
 
 // Rotates about X-axis
@@ -13,7 +14,9 @@ func Rotate2D(angle float64) Matrix {
 // [0 s c ]
 func Rotate3DX(angle float64) Matrix {
 	sin, cos := math.Sin(angle), math.Cos(angle)
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, 0, 0, cos, -sin, 0, sin, cos}, FLOAT64), FLOAT64, 3, 3)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, 0,
+		0, cos, -sin,
+		0, sin, cos}, FLOAT64), FLOAT64, 3, 3)
 }
 
 // Rotates around Y-axis
@@ -22,7 +25,9 @@ func Rotate3DX(angle float64) Matrix {
 // [s 0 c ]
 func Rotate3DY(angle float64) Matrix {
 	sin, cos := math.Sin(angle), math.Cos(angle)
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, 0, sin, 0, 1, 0, -sin, 0, cos}, FLOAT64), FLOAT64, 3, 3)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, 0, sin,
+		0, 1, 0,
+		-sin, 0, cos}, FLOAT64), FLOAT64, 3, 3)
 }
 
 // Rotates about Z-axis
@@ -31,22 +36,31 @@ func Rotate3DY(angle float64) Matrix {
 // [0 0 1 ]
 func Rotate3DZ(angle float64) Matrix {
 	sin, cos := math.Sin(angle), math.Cos(angle)
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, -sin, 0, -sin, cos, 0, 0, 0, 1}, FLOAT64), FLOAT64, 3, 3)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, -sin, 0,
+		-sin, cos, 0,
+		0, 0, 1}, FLOAT64), FLOAT64, 3, 3)
 }
 
 // Returns a 2D homogeneous (3x3) transformation matrix
 func Transform2D(Tx, Ty float64) Matrix {
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, Tx, 0, 1, Ty, 0, 0, 1}, FLOAT64), FLOAT64, 3, 3)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, Tx,
+		0, 1, Ty,
+		0, 0, 1}, FLOAT64), FLOAT64, 3, 3)
 }
 
 // Returns a 3D homogeneous (4x4) transformation matrix
 func Transform3D(Tx, Ty, Tz float64) Matrix {
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, 0, Tx, 0, 1, 0, Ty, 0, 0, 1, Tz, 0, 0, 0, 1}, FLOAT64), FLOAT64, 4, 4)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, 0, Tx,
+		0, 1, 0, Ty,
+		0, 0, 1, Tz,
+		0, 0, 0, 1}, FLOAT64), FLOAT64, 4, 4)
 }
 
 func HomogRotate2D(angle float64) Matrix {
 	sin, cos := math.Sin(angle), math.Cos(angle)
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, -sin, 0, sin, cos, 0, 0, 0, 1}, FLOAT64), FLOAT64, 2, 2)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, -sin, 0,
+		sin, cos, 0,
+		0, 0, 1}, FLOAT64), FLOAT64, 3, 3)
 }
 
 // Rotates about X-axis
@@ -55,7 +69,10 @@ func HomogRotate2D(angle float64) Matrix {
 // [0 s c ]
 func HomogRotate3DX(angle float64) Matrix {
 	sin, cos := math.Sin(angle), math.Cos(angle)
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, 0, 0, 0, cos, -sin, 0, 0, sin, cos, 0, 0, 0, 0, 1}, FLOAT64), FLOAT64, 4, 4)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{1, 0, 0, 0,
+		0, cos, -sin, 0,
+		0, sin, cos, 0,
+		0, 0, 0, 1}, FLOAT64), FLOAT64, 4, 4)
 }
 
 // Rotates around Y-axis
@@ -64,7 +81,10 @@ func HomogRotate3DX(angle float64) Matrix {
 // [s 0 c ]
 func HomogRotate3DY(angle float64) Matrix {
 	sin, cos := math.Sin(angle), math.Cos(angle)
-	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, 0, sin, 0, 0, 1, 0, 0, -sin, 0, cos, 0, 0, 0, 0, 1}, FLOAT64), FLOAT64, 4, 4)
+	return *unsafeMatrixFromSlice(ScalarSlice([]interface{}{cos, 0, sin, 0,
+		0, 1, 0, 0,
+		-sin, 0, cos, 0,
+		0, 0, 0, 1}, FLOAT64), FLOAT64, 4, 4)
 }
 
 // Rotates about Z-axis
