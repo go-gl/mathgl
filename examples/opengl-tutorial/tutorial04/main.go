77cadc5e0e276b4b2cbacec8bea7c44b931c7a50
Jragonmiris <Jragonmiris@gmail.com>
2013-02-23 10:30:22 -0700

    Ported tutorial 4

---
+++ examples/opengl-tutorial/tutorial04/main.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Jragonmiris/mathgl"
+	"github.com/go-gl/gl"
+	"github.com/go-gl/glfw"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		return
+	}
+
+	defer glfw.Terminate()
+
+	glfw.OpenWindowHint(glfw.FsaaSamples, 4)
+	glfw.OpenWindowHint(glfw.OpenGLVersionMajor, 3)
+	glfw.OpenWindowHint(glfw.OpenGLVersionMinor, 3)
+	glfw.OpenWindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+
+	if err := glfw.OpenWindow(1024, 768, 0, 0, 0, 0, 32, 0, glfw.Windowed); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		return
+	}
+
+	// glewExperimental=true ?
+	gl.Init() // Can't find gl.GLEW_OK or any variation, not sure how to check if this worked
+
+	glfw.SetWindowTitle("Tutorial 04")
+
+	glfw.Enable(glfw.StickyKeys)
+	gl.ClearColor(0., 0., 0.4, 0.)
+
+	gl.Enable(gl.DEPTH_TEST)
+	gl.DepthFunc(gl.LESS)
+
+	vertexArray := gl.GenVertexArray()
+	defer vertexArray.Delete()
+	vertexArray.Bind()
+
+	prog := MakeProgram("TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader")
+	defer prog.Delete()
+
+	matrixID := prog.GetUniformLocation("MVP")
+
+	Projection := mathgl.Perspective(45.0, 4.0/3.0, 0.1, 100.0)
+
+	View := mathgl.LookAt(4.0, 3.0, -3.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+
+	Model := mathgl.Identity(4, mathgl.FLOAT64)
+
+	MVP := Projection.Mul(View).Mul(Model)                   // Remember, transform multiplication order is "backwards"
+	mvpArray := MVP.AsCMOArray(mathgl.FLOAT32).([16]float32) // OpenGL likes CMO
+
+	vBufferData := [...]float32{
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		1.0, -1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		-1.0, -1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		-1.0, -1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, -1.0,
+		1.0, -1.0, -1.0,
+		1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0,
+		1.0, 1.0, 1.0,
+		1.0, 1.0, -1.0,
+		-1.0, 1.0, -1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, -1.0,
+		-1.0, 1.0, 1.0,
+		1.0, 1.0, 1.0,
+		-1.0, 1.0, 1.0,
+		1.0, -1.0, 1.0}
+
+	colorBufferData := [...]float32{
+		0.583, 0.771, 0.014,
+		0.609, 0.115, 0.436,
+		0.327, 0.483, 0.844,
+		0.822, 0.569, 0.201,
+		0.435, 0.602, 0.223,
+		0.310, 0.747, 0.185,
+		0.597, 0.770, 0.761,
+		0.559, 0.436, 0.730,
+		0.359, 0.583, 0.152,
+		0.483, 0.596, 0.789,
+		0.559, 0.861, 0.639,
+		0.195, 0.548, 0.859,
+		0.014, 0.184, 0.576,
+		0.771, 0.328, 0.970,
+		0.406, 0.615, 0.116,
+		0.676, 0.977, 0.133,
+		0.971, 0.572, 0.833,
+		0.140, 0.616, 0.489,
+		0.997, 0.513, 0.064,
+		0.945, 0.719, 0.592,
+		0.543, 0.021, 0.978,
+		0.279, 0.317, 0.505,
+		0.167, 0.620, 0.077,
+		0.347, 0.857, 0.137,
+		0.055, 0.953, 0.042,
+		0.714, 0.505, 0.345,
+		0.783, 0.290, 0.734,
+		0.722, 0.645, 0.174,
+		0.302, 0.455, 0.848,
+		0.225, 0.587, 0.040,
+		0.517, 0.713, 0.338,
+		0.053, 0.959, 0.120,
+		0.393, 0.621, 0.362,
+		0.673, 0.211, 0.457,
+		0.820, 0.883, 0.371,
+		0.982, 0.099, 0.879}
+
+	//elBufferData := [...]uint8{0, 1, 2} // Not sure why this is here
+
+	vertexBuffer := gl.GenBuffer()
+	defer vertexBuffer.Delete()
+	vertexBuffer.Bind(gl.ARRAY_BUFFER)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vBufferData)*4, &vBufferData, gl.STATIC_DRAW)
+
+	colorBuffer := gl.GenBuffer()
+	defer colorBuffer.Delete()
+	colorBuffer.Bind(gl.ARRAY_BUFFER)
+	gl.BufferData(gl.ARRAY_BUFFER, len(colorBufferData)*4, &colorBufferData, gl.STATIC_DRAW)
+
+	// Equivalent to a do... while
+	for ok := true; ok; ok = (glfw.Key(glfw.KeyEsc) != glfw.KeyPress && glfw.WindowParam(glfw.Opened) == gl.TRUE) {
+		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+
+		prog.Use()
+
+		matrixID.UniformMatrix4fv(false, mvpArray)
+
+		vertexAttrib := gl.AttribLocation(0)
+		vertexAttrib.EnableArray()
+		vertexBuffer.Bind(gl.ARRAY_BUFFER)
+		vertexAttrib.AttribPointer(3, gl.FLOAT, false, 0, nil)
+
+		colorAttrib := gl.AttribLocation(1)
+		colorAttrib.EnableArray()
+		colorBuffer.Bind(gl.ARRAY_BUFFER)
+		colorAttrib.AttribPointer(3, gl.FLOAT, false, 0, nil)
+
+		gl.DrawArrays(gl.TRIANGLES, 0, 12*3)
+
+		vertexAttrib.DisableArray()
+		colorAttrib.DisableArray()
+
+		glfw.SwapBuffers()
+	}
+
+}
+
+func MakeProgram(vertFname, fragFname string) gl.Program {
+	vertSource, err := ioutil.ReadFile(vertFname)
+	if err != nil {
+		panic(err)
+	}
+
+	fragSource, err := ioutil.ReadFile(fragFname)
+	if err != nil {
+		panic(err)
+	}
+
+	vertShader, fragShader := gl.CreateShader(gl.VERTEX_SHADER), gl.CreateShader(gl.FRAGMENT_SHADER)
+	vertShader.Source(string(vertSource))
+	fragShader.Source(string(fragSource))
+
+	vertShader.Compile()
+	fragShader.Compile()
+
+	prog := gl.CreateProgram()
+	prog.AttachShader(vertShader)
+	prog.AttachShader(fragShader)
+	prog.Link()
+	prog.Validate()
+	fmt.Println(prog.GetInfoLog())
+
+	return prog
+}
