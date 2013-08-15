0f1eb23e5d118c99b80b51c61efc8d23fe5ce411
Jsor <Jragonmiris@gmail.com>
2013-08-15 03:16:49 -0700

    Updates

--- projectf.go
+++ projectf.go
@@ -5,31 +5,32 @@ import (
 	"math"
 )
 
-func Ortho(left, right, bottom, top, near, far float64) Mat4f {
+func Ortho(left, right, bottom, top, near, far float32) Mat4f {
 	rml, tmb, fmn := (right - left), (top - bottom), (far - near)
 
 	return Mat4f{float32(2. / rml), 0, 0, 0, 0, float32(2. / tmb), 0, 0, 0, 0, float32(-2. / fmn), 0, float32(-(right + left) / rml), float32(-(top + bottom) / tmb), float32(-(far + near) / fmn), 1}
 }
 
-func Ortho2D(left, right, top, bottom float64) Mat4f {
+// Equivalent to Ortho with the near and far planes being -1 and 1, respectively
+func Ortho2D(left, right, top, bottom float32) Mat4f {
 	return Ortho(left, right, top, bottom, -1, 1)
 }
 
-func Perspective(fovy, aspect, near, far float64) Mat4f {
+func Perspective(fovy, aspect, near, far float32) Mat4f {
 	fovy = (fovy * math.Pi) / 180.0 // convert from degrees to radians
-	nmf, f := near-far, 1./math.Tan(fovy/2)
+	nmf, f := near-far, float32(1./math.Tan(float64(fovy)/2.0))
 
 	return Mat4f{float32(f / aspect), 0, 0, 0, 0, float32(f), 0, 0, 0, 0, float32((near + far) / nmf), -1, 0, 0, float32((2. * far * near) / nmf), 0}
 }
 
-func Frustum(left, right, bottom, top, near, far float64) Mat4f {
+func Frustum(left, right, bottom, top, near, far float32) Mat4f {
 	rml, tmb, fmn := (right - left), (top - bottom), (far - near)
 	A, B, C, D := (right+left)/rml, (top+bottom)/tmb, -(far+near)/fmn, (2*far*near)/fmn
 
 	return Mat4f{float32((2. * near) / rml), 0, 0, 0, 0, float32((2. * near) / tmb), 0, 0, float32(A), float32(B), float32(C), -1, 0, 0, float32(D), 0}
 }
 
-func LookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ float64) Mat4f {
+func LookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, upX, upY, upZ float32) Mat4f {
 	F := Vec3f{
 		float32(centerX - eyeX),
 		float32(centerY - eyeY),
@@ -64,9 +65,13 @@ func LookAtV(eye, center, up Vec3f) Mat4f {
 
 	M := Mat4f{s[0], u[0], -f[0], 0, s[1], u[1], -f[1], 0, s[2], u[2], -f[2], 0, 0, 0, 0, 1}
 
-	return M.Mul4(Translate3D(float64(-eye[0]), float64(-eye[1]), float64(-eye[2])))
+	return M.Mul4(Translate3D(float32(-eye[0]), float32(-eye[1]), float32(-eye[2])))
 }
 
+// Transform a set of coordinates from object space (in obj) to window coordinates (with depth)
+//
+// Window coordinates are continuous, not discrete (well, as continuous as an IEEE Floating Point can be), so you won't get exact pixel locations
+// without rounding or similar
 func Projectf(obj Vec3f, modelview, projection Mat4f, initialX, initialY, width, height int) (win Vec3f) {
 	obj4 := Vec4f{obj[0], obj[1], obj[2], 1.0}
 
@@ -78,6 +83,10 @@ func Projectf(obj Vec3f, modelview, projection Mat4f, initialX, initialY, width,
 	return win
 }
 
+// Transform a set of window coordinates to object space. If your MVP (projection.Mul(modelview) matrix is not invertible, this will return an error
+//
+// Note that the projection may not be perfect if you use strict pixel locations rather than the exact values given by Projectf.
+// (It's still unlikely to be perfect due to precision errors, but it will be closer)
 func UnProjectf(win Vec3f, modelview, projection Mat4f, initialX, initialY, width, height int) (obj Vec3f, err error) {
 	inv := projection.Mul4(modelview).Inv()
 	blank := Mat4f{}
--- transformf.go
+++ transformf.go
@@ -5,9 +5,9 @@ import "math"
 // Rotate2D returns a rotation Matrix about a angle in 2-D space. Specifically about the origin.
 // It is a 2x2 matrix, if you need a 3x3 for Homogeneous math (e.g. composition with a Translation matrix)
 // see HomogRotate2D
-func Rotate2D(angle float64) Mat2f {
+func Rotate2D(angle float32) Mat2f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 	return Mat2f{cos, sin, -sin, cos}
 }
 
@@ -17,9 +17,9 @@ func Rotate2D(angle float64) Mat2f {
 //    [1 0 0]
 //    [0 c -s]
 //    [0 s c ]
-func Rotate3DX(angle float64) Mat3f {
+func Rotate3DX(angle float32) Mat3f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 
 	return Mat3f{1, 0, 0, 0, cos, sin, 0, -sin, cos}
 }
@@ -30,9 +30,9 @@ func Rotate3DX(angle float64) Mat3f {
 //    [c 0 s]
 //    [0 1 0]
 //    [s 0 c ]
-func Rotate3DY(angle float64) Mat3f {
+func Rotate3DY(angle float32) Mat3f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 
 	return Mat3f{cos, 0, -sin, 0, 1, 0, sin, 0, cos}
 }
@@ -43,9 +43,9 @@ func Rotate3DY(angle float64) Mat3f {
 //    [c -s 0]
 //    [s c 0]
 //    [0 0 1 ]
-func Rotate3DZ(angle float64) Mat3f {
+func Rotate3DZ(angle float32) Mat3f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 
 	return Mat3f{cos, sin, 0, -sin, cos, 0, 0, 0, 1}
 }
@@ -55,7 +55,7 @@ func Rotate3DZ(angle float64) Mat3f {
 //    [[1, 0, Tx]]
 //    [[0, 1, Ty]]
 //    [[0, 0, 1 ]]
-func Translate2D(Tx, Ty float64) Mat3f {
+func Translate2D(Tx, Ty float32) Mat3f {
 	return Mat3f{1, 0, 0, 0, 1, 0, float32(Tx), float32(Ty), 1}
 }
 
@@ -66,36 +66,36 @@ func Translate2D(Tx, Ty float64) Mat3f {
 //    [[0, 1, 0, Ty]]
 //    [[0, 0, 1, Tz]]
 //    [[0, 0, 0, 1 ]]
-func Translate3D(Tx, Ty, Tz float64) Mat4f {
+func Translate3D(Tx, Ty, Tz float32) Mat4f {
 	return Mat4f{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, float32(Tx), float32(Ty), float32(Tz), 1}
 }
 
 // Same as Rotate2D, except homogeneous (3x3 with the extra row/col being all zeroes with a one in the bottom right)
-func HomogRotate2D(angle float64) Mat3f {
+func HomogRotate2D(angle float32) Mat3f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 	return Mat3f{cos, sin, 0, -sin, cos, 0, 0, 0, 1}
 }
 
 // Same as Rotate3DX, except homogeneous (4x4 with the extra row/col being all zeroes with a one in the bottom right)
-func HomogRotate3DX(angle float64) Mat4f {
+func HomogRotate3DX(angle float32) Mat4f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 
 	return Mat4f{1, 0, 0, 0, 0, cos, sin, 0, 0, -sin, cos, 0, 0, 0, 0, 1}
 }
 
 // Same as Rotate3DY, except homogeneous (4x4 with the extra row/col being all zeroes with a one in the bottom right)
-func HomogRotate3DY(angle float64) Mat4f {
+func HomogRotate3DY(angle float32) Mat4f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 	return Mat4f{cos, 0, -sin, 0, 0, 1, 0, 0, sin, 0, cos, 0, 0, 0, 0, 1}
 }
 
 // Same as Rotate3DZ, except homogeneous (4x4 with the extra row/col being all zeroes with a one in the bottom right)
-func HomogRotate3DZ(angle float64) Mat4f {
+func HomogRotate3DZ(angle float32) Mat4f {
 	angle = (angle * math.Pi) / 180.0
-	sin, cos := float32(math.Sin(angle)), float32(math.Cos(angle))
+	sin, cos := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 	return Mat4f{cos, sin, 0, 0, -sin, cos, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
 }
 
@@ -104,7 +104,7 @@ func HomogRotate3DZ(angle float64) Mat4f {
 // [[ 0     , scaleY, 0     , 0 ]]
 // [[ 0     , 0     , scaleZ, 0 ]]
 // [[ 0     , 0     , 0     , 1 ]]
-func Scale3D(scaleX, scaleY, scaleZ float64) Mat4f {
+func Scale3D(scaleX, scaleY, scaleZ float32) Mat4f {
 
 	return Mat4f{float32(scaleX), 0, 0, 0, 0, float32(scaleY), 0, 0, 0, 0, float32(scaleZ), 0, 0, 0, 0, 1}
 }
@@ -113,33 +113,33 @@ func Scale3D(scaleX, scaleY, scaleZ float64) Mat4f {
 // [[ scaleX, 0     , 0 ]]
 // [[ 0     , scaleY, 0 ]]
 // [[ 0     , 0     , 1 ]]
-func Scale2D(scaleX, scaleY float64) Mat3f {
+func Scale2D(scaleX, scaleY float32) Mat3f {
 	return Mat3f{float32(scaleX), 0, 0, 0, float32(scaleY), 0, 0, 0, 1}
 }
 
 // ShearX2D creates a homogeneous 2D shear matrix along the X-axis
-func ShearX2D(shear float64) Mat3f {
+func ShearX2D(shear float32) Mat3f {
 	return Mat3f{1, 0, 0, float32(shear), 1, 0, 0, 0, 1}
 }
 
 // ShearY2D creates a homogeneous 2D shear matrix along the Y-axis
-func ShearY2D(shear float64) Mat3f {
+func ShearY2D(shear float32) Mat3f {
 	return Mat3f{1, float32(shear), 0, 0, 1, 0, 0, 0, 1}
 }
 
 // ShearX3D creates a homogeneous 3D shear matrix along the X-axis
-func ShearX3D(shearY, shearZ float64) Mat4f {
+func ShearX3D(shearY, shearZ float32) Mat4f {
 
 	return Mat4f{1, float32(shearY), float32(shearZ), 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}
 }
 
 // ShearY3D creates a homogeneous 3D shear matrix along the Y-axis
-func ShearY3D(shearX, shearZ float64) Mat4f {
+func ShearY3D(shearX, shearZ float32) Mat4f {
 	return Mat4f{1, 0, 0, 0, float32(shearX), 1, float32(shearZ), 0, 0, 0, 1, 0, 0, 0, 0, 1}
 }
 
 // ShearZ3D creates a homogeneous 3D shear matrix along the Z-axis
-func ShearZ3D(shearX, shearY float64) Mat4f {
+func ShearZ3D(shearX, shearY float32) Mat4f {
 	return Mat4f{1, 0, 0, 0, 0, 1, 0, 0, float32(shearX), float32(shearY), 1, 0, 0, 0, 0, 1}
 }
 
@@ -152,9 +152,9 @@ func ShearZ3D(shearX, shearY float64) Mat4f {
 //    [[ xy(c-1)+zs, y^2(c-1)+c, yz(c-1)-xs, 0 ]]
 //    [[ xz(c-1)-ys, yz(c-1)+xs, z^2(c-1)+c, 0 ]]
 //    [[ 0         , 0         , 0         , 1 ]]
-func HomogRotate3D(angle float64, axis Vec3f) Mat4f {
+func HomogRotate3D(angle float32, axis Vec3f) Mat4f {
 	x, y, z := axis[0], axis[1], axis[2]
-	s, c := float32(math.Sin(angle)), float32(math.Cos(angle))
+	s, c := float32(math.Sin(float64(angle))), float32(math.Cos(float64(angle)))
 	k := 1 - c
 
 	return Mat4f{x*x*k + c, x*y*k + z*s, x*z*k - y*s, 0, x*y*k - z*s, y*y*k + c, y*z*k + x*s, 0, x*z*k + y*s, y*z*k - x*s, z*z*k + c, 0, 0, 0, 0, 1}
