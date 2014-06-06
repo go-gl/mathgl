17075ab6ae5b401c42f46122748f97469874d146
Jsor <Jragonmiris@gmail.com>
2014-06-06 05:30:54 -0700

    Added vecn

---
+++ mgl32/vecn.go
@@ -0,0 +1,228 @@
+package mgl32
+
+import (
+	"math"
+)
+
+var reallocCallback func([]float32)
+
+// Registers a callback that will be called when a VecN or
+// MatMN has to reallocate its underlying slice. The old slice
+// will be sent to the receiver.
+//
+// This is useful for memory pools; this callback does not discriminate,
+// if you register the same slice multiple times, you get it back
+// every time it's thrown away.
+//
+// Note that registering on this callback does not prevent garbage
+// collection on its own! The callback won't be called if you simply let the
+// VecN or MatMN go out of scope!
+//
+// Only one callback may be registered with the package, the most recent call prevails.
+// This is not thread safe, and ideally should only be called at initialization.
+func RegisterReallocCallback(cb func([]float32)) {
+
+}
+
+// A vector of N elements backed by a slice
+type VecN struct {
+	vec []float32
+}
+
+// Creates a new vector with backing slice initial.
+// If initial is nil, this vector will generate its own
+// slice when needed.
+func NewVecN(initial []float32) *VecN {
+	return &VecN{initial}
+}
+
+// Returns the raw slice backing the VecN
+func (vn VecN) Raw() []float32 {
+	return vn.vec
+}
+
+// Grows the slice by the desired amount
+func (vn *VecN) grow(size int) {
+	if len(vn.vec)+size > cap(vn.vec) {
+		tmp := make([]float32, len(vn.vec), len(vn.vec)*2)
+		copy(tmp, vn.vec)
+		if reallocCallback != nil {
+			reallocCallback(vn.vec)
+		}
+
+		vn.vec = tmp
+
+		return
+	}
+
+	vn.vec = vn.vec[:len(vn.vec)+size]
+}
+
+// Appends to the slice, calling the realloc callback if necessary
+func (vn *VecN) append(toAdd []float32) {
+	if reallocCallback != nil && len(vn.vec)+len(toAdd) > cap(vn.vec) {
+		// Done this way so the callback doesn't do something to our slice before
+		// it's copied into the new one
+		tmp = vn.vec
+		vn.vec = append(vn.vec, toAdd...)
+		reallocCallback(tmp)
+		return
+	}
+
+	vn.vec = append(vn.vec, toAdd...)
+}
+
+// Resizes the underlying slice to the desired amount, reallocating
+// if necessary. This does not zero any values.
+func (vn *VecN) Resize(n int) {
+	if n <= len(vn.vec) {
+		vn.vec = vn.vec[:n]
+		return
+	}
+
+	vn.grow(n - len(vn.vec))
+}
+
+// Sets the vector's backing slice to the given
+// new one.
+func (vn *VecN) SetBackingSlice(newSlice []float32) {
+	vn.vec = newSlice
+}
+
+// Return the len of the vector's underlying slice.
+// This is not titled Len because it conflicts the package's
+// convention of calling the Norm the Len.
+func (vn *VecN) Size() int {
+	return len(vn.vec)
+}
+
+// Returns the cap of the vector's underlying slice.
+func (vn *VecN) Cap() int {
+	return cap(vn.vec)
+}
+
+// Sets the vector's size to n and zeroes out the vector.
+// If n is bigger than the vector's size, it will realloc.
+func (vn *VecN) Zero(n int) {
+	vn.resize(n)
+	for i := range vn.vec {
+		vn.vec[i] = 0
+	}
+}
+
+// Adds vn and addend, storing the result in dst.
+// If dst does not have sufficient size it will be resized
+// Dst may be one of the other arguments. If dst is nil, it will be allocated.
+// The value returned is dst, for easier method chaining
+//
+// If vn and addend are not the same size, this function will add min(vn.Size(), addend.Size())
+// elements.
+func (vn *VecN) Add(dst *VecN, addend *VecN) *VecN {
+	size := intMin(len(vn.vec), len(addend.vec))
+	dst.Resize(size)
+
+	for i := 0; i < size; i++ {
+		dst.vec[i] = vn.vec[i] + addend.vec[i]
+	}
+
+	return dst
+}
+
+// Subtracts addend from vn, storing the result in dst.
+// If dst does not have sufficient size it will be resized
+// Dst may be one of the other arguments. If dst is nil, it will be allocated.
+// The value returned is dst, for easier method chaining
+//
+// If vn and addend are not the same size, this function will add min(vn.Size(), addend.Size())
+// elements.
+func (vn *VecN) Sub(dst *VecN, addend *VecN) *VecN {
+	size := intMin(len(vn.vec), len(addend.vec))
+	dst.Resize(size)
+
+	for i := 0; i < size; i++ {
+		dst.vec[i] = vn.vec[i] - addend.vec[i]
+	}
+
+	return dst
+}
+
+// Takes the binary cross product of vn and other, and stores it in dst.
+// If either vn or other are not of size 3 this function will panic
+//
+// If dst is not of sufficient size, or is nil, a new slice is allocated.
+// Dst is permitted to be one of the other arguments
+func (vn *VecN) Cross(dst *VecN, other *VecN) *VecN {
+	if len(vn.vec) != 3 || len(other.vec) != 3 {
+		panic("Cannot take binary cross product of non-3D elements (7D cross product not implemented)")
+	}
+
+	dst.Resize(3)
+	dst.vec[0], dst.vec[1], dst.vec[2] = vn.vec[1]*other.vec[2]-vn.vec[2]*other.vec[1], vn.vec[2]*other.vec[0]-vn.vec[0]*other.vec[2], vn.vec[0]*other.vec[1]-vn.vec[1]*other.vec[0]
+
+	return dst
+}
+
+func intMin(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
+// Computes the dot product of two VecNs, if
+// the two vectors are not of the same length -- this
+// will return NaN.
+func (vn *VecN) Dot(other *VecN) float32 {
+	if len(vn.vec) != len(other.vec) {
+		return float32(math.NaN())
+	}
+
+	result := 0.0
+	for i, el := range vn.vec {
+		result += el * other.vec[i]
+	}
+
+	return result
+}
+
+// Computes the vector length (also called the Norm) of the
+// vector. Equivalent to math.Sqrt(vn.Dot(vn)) with the appropriate
+// type conversions.
+func (vn *VecN) Len() float32 {
+	if len(vn.vec) == 0 {
+		return 0
+	}
+
+	return float32(math.Sqrt(float64(vn.Dot(vn))))
+}
+
+// Normalizes the vector and stores the result in dst, which
+// will be returned. Dst will be appropraitely resized to the
+// size of vn.
+//
+// The destination can be vn itself and nothing will go wrong.
+//
+// This is equivalent to vn.Mul(dst, vn.Len())
+func (vn *VecN) Normalize(dst *VecN) *VecN {
+
+	return vn.Mul(dst, vn.Len())
+}
+
+// Multiplied the vector by some scalar value and stores the result in dst, which
+// will be returned. Dst will be appropraitely resized to the
+// size of vn.
+//
+// The destination can be vn itself and nothing will go wrong.
+//
+// This is equivalent to vn.Mul(dst, vn.Len())
+func (vn *VecN) Mul(dst *VecN, c float32) *VecN {
+	dst.Resize(len(vn.vec))
+
+	length := vn.Len()
+	for i, el := range vn.vec {
+		dst.vec[1] = vn.vec[i] / length
+	}
+
+	return dst
+}
