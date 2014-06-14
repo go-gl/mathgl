c1b97d38d59bb05da2e546428cef43e02dac65f7
Jsor <Jragonmiris@gmail.com>
2014-06-14 04:57:30 -0700

    Added dot product test for VecN

--- mgl32/vecn_test.go
+++ mgl32/vecn_test.go
@@ -20,3 +20,19 @@ func TestVecNCross(t *testing.T) {
 		t.Errorf("VecN cross product is incorrect. Got: %v; Expected: %v", result, correct)
 	}
 }
+
+func TestVecNDot(t *testing.T) {
+	v1 := Vec3{1, 3, 5}
+	v2 := Vec3{2, 4, 6}
+
+	correct := v1.Dot(v2)
+
+	v1n := NewBackedVecN(v1[:])
+	v2n := NewBackedVecN(v2[:])
+
+	result := v1n.Dot(v2n)
+
+	if !FloatEqualThreshold(correct, result, 1e-4) {
+		t.Errorf("Dot product doesn't work for VecN. Got: %v, Expected: %v", result, correct)
+	}
+}
