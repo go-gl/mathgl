e2721366e5ec71ae3cbef000395b02be37b8b228
Arne Döring <arne.doering@gmx.net>
2014-06-15 00:23:55 +0200

    adapted test files for changed api

--- mgl64/mat_test.go
+++ mgl64/mat_test.go
@@ -26,11 +26,11 @@ func TestMulIdent(t *testing.T) {
 
 // M>N
 func TestMatRowsMGTN(t *testing.T) {
-	rows := [2]Vec3{
-		Vec3{1, 2, 3},
-		Vec3{4, 5, 6},
-	}
-	m1 := Mat2x3FromRows(rows)
+	row0 := Vec3{1, 2, 3}
+	row1 := Vec3{4, 5, 6}
+	rows := [2]Vec3{row0, row1}
+
+	m1 := Mat2x3FromRows(rows[0], rows[1])
 
 	t.Logf("2x3 matrix as built from rows: %v", m1)
 	for r := 0; r < 2; r++ {
@@ -41,13 +41,14 @@ func TestMatRowsMGTN(t *testing.T) {
 		}
 	}
 
-	r2 := m1.Rows()
+	row0, row1 = m1.Rows()
+	rows2 := [2]Vec3{row0, row1}
 
-	t.Logf("2x3 matrix returned rows: %v", r2)
+	t.Logf("2x3 matrix returned rows: %v", rows2)
 	for r := 0; r < 2; r++ {
 		for c := 0; c < 3; c++ {
-			if !FloatEqualThreshold(r2[r][c], rows[r][c], 1e-5) {
-				t.Errorf("Matrix element at (%d,%d) wrong when rows are gotten. Got: %f, Expected: %f", r, c, r2[r][c], rows[r][c])
+			if !FloatEqualThreshold(rows2[r][c], rows[r][c], 1e-5) {
+				t.Errorf("Matrix element at (%d,%d) wrong when rows are gotten. Got: %f, Expected: %f", r, c, rows2[r][c], rows[r][c])
 			}
 		}
 	}
@@ -55,13 +56,15 @@ func TestMatRowsMGTN(t *testing.T) {
 
 // M<N
 func TestMatRowsMLTN(t *testing.T) {
-	rows := [4]Vec3{
-		Vec3{1, 2, 3},
-		Vec3{4, 5, 6},
-		Vec3{7, 8, 9},
-		Vec3{10, 11, 12},
-	}
-	m1 := Mat4x3FromRows(rows)
+
+	v0 := Vec3{1, 2, 3}
+	v1 := Vec3{4, 5, 6}
+	v2 := Vec3{7, 8, 9}
+	v3 := Vec3{10, 11, 12}
+
+	rows := [4]Vec3{v0, v1, v2, v3}
+
+	m1 := Mat4x3FromRows(v0, v1, v2, v3)
 
 	t.Logf("4x3 matrix as built from rows: %v", m1)
 	for r := 0; r < 4; r++ {
@@ -72,7 +75,8 @@ func TestMatRowsMLTN(t *testing.T) {
 		}
 	}
 
-	r2 := m1.Rows()
+	v0, v1, v2, v3 = m1.Rows()
+	r2 := [4]Vec3{v0, v1, v2, v3}
 
 	t.Logf("4x3 matrix returned rows: %v", r2)
 	for r := 0; r < 4; r++ {
@@ -86,13 +90,13 @@ func TestMatRowsMLTN(t *testing.T) {
 
 // Square matrix
 func TestMatRowsM(t *testing.T) {
-	rows := [4]Vec4{
-		Vec4{1, 2, 3, 4},
-		Vec4{5, 6, 7, 8},
-		Vec4{9, 10, 11, 12},
-		Vec4{13, 14, 15, 16},
-	}
-	m1 := Mat4FromRows(rows)
+
+	v0 := Vec4{1, 2, 3, 4}
+	v1 := Vec4{5, 6, 7, 8}
+	v2 := Vec4{9, 10, 11, 12}
+	v3 := Vec4{13, 14, 15, 16}
+	rows := [4]Vec4{v0, v1, v2, v3}
+	m1 := Mat4FromRows(v0, v1, v2, v3)
 
 	t.Logf("4x4 matrix as built from rows: %v", m1)
 	for r := 0; r < 4; r++ {
@@ -103,7 +107,8 @@ func TestMatRowsM(t *testing.T) {
 		}
 	}
 
-	r2 := m1.Rows()
+	v0, v1, v2, v3 = m1.Rows()
+	r2 := [4]Vec4{v0, v1, v2, v3}
 
 	t.Logf("4x4 matrix returned rows: %v", r2)
 	for r := 0; r < 4; r++ {
@@ -117,11 +122,11 @@ func TestMatRowsM(t *testing.T) {
 
 // M<N
 func TestMatColsMLTN(t *testing.T) {
-	cols := [2]Vec3{
-		Vec3{1, 2, 3},
-		Vec3{4, 5, 6},
-	}
-	m1 := Mat3x2FromCols(cols)
+
+	v0 := Vec3{1, 2, 3}
+	v1 := Vec3{4, 5, 6}
+	cols := [2]Vec3{v0, v1}
+	m1 := Mat3x2FromCols(v0, v1)
 
 	t.Logf("3x2 matrix as built from cols: %v", m1)
 	for r := 0; r < 3; r++ {
@@ -132,7 +137,8 @@ func TestMatColsMLTN(t *testing.T) {
 		}
 	}
 
-	r2 := m1.Cols()
+	v0, v1 = m1.Cols()
+	r2 := [2]Vec3{v0, v1}
 
 	t.Logf("3x2 matrix returned cols: %v", r2)
 	for r := 0; r < 3; r++ {
@@ -146,13 +152,13 @@ func TestMatColsMLTN(t *testing.T) {
 
 // M>N
 func TestMatColsMGTN(t *testing.T) {
-	cols := [4]Vec3{
-		Vec3{1, 2, 3},
-		Vec3{4, 5, 6},
-		Vec3{7, 8, 9},
-		Vec3{10, 11, 12},
-	}
-	m1 := Mat3x4FromCols(cols)
+
+	v0 := Vec3{1, 2, 3}
+	v1 := Vec3{4, 5, 6}
+	v2 := Vec3{7, 8, 9}
+	v3 := Vec3{10, 11, 12}
+	cols := [4]Vec3{v0, v1, v2, v3}
+	m1 := Mat3x4FromCols(v0, v1, v2, v3)
 
 	t.Logf("3x4 matrix as built from cols: %v", m1)
 	for r := 0; r < 3; r++ {
@@ -163,7 +169,8 @@ func TestMatColsMGTN(t *testing.T) {
 		}
 	}
 
-	r2 := m1.Cols()
+	v0, v1, v2, v3 = m1.Cols()
+	r2 := [4]Vec3{v0, v1, v2, v3}
 
 	t.Logf("3x4 matrix returned cols: %v", r2)
 	for r := 0; r < 3; r++ {
@@ -177,13 +184,12 @@ func TestMatColsMGTN(t *testing.T) {
 
 // Square matrix
 func TestMatColsM(t *testing.T) {
-	cols := [4]Vec4{
-		Vec4{1, 2, 3, 4},
-		Vec4{5, 6, 7, 8},
-		Vec4{9, 10, 11, 12},
-		Vec4{13, 14, 15, 16},
-	}
-	m1 := Mat4FromCols(cols)
+	v0 := Vec4{1, 2, 3, 4}
+	v1 := Vec4{5, 6, 7, 8}
+	v2 := Vec4{9, 10, 11, 12}
+	v3 := Vec4{13, 14, 15, 16}
+	cols := [4]Vec4{v0, v1, v2, v3}
+	m1 := Mat4FromCols(v0, v1, v2, v3)
 
 	t.Logf("4x4 matrix as built from cols: %v", m1)
 	for r := 0; r < 4; r++ {
@@ -194,7 +200,8 @@ func TestMatColsM(t *testing.T) {
 		}
 	}
 
-	r2 := m1.Cols()
+	v0, v1, v2, v3 = m1.Cols()
+	r2 := [4]Vec4{v0, v1, v2, v3}
 
 	t.Logf("4x4 matrix returned cols: %v", r2)
 	for r := 0; r < 4; r++ {
@@ -207,17 +214,17 @@ func TestMatColsM(t *testing.T) {
 }
 
 func TestTransposeTall(t *testing.T) {
-	m := Mat3x2FromCols([2]Vec3{
+	m := Mat3x2FromCols(
 		Vec3{1, 2, 3},
 		Vec3{4, 5, 6},
-	})
+	)
 
 	transpose := m.Transpose()
 
-	correct := Mat2x3FromRows([2]Vec3{
+	correct := Mat2x3FromRows(
 		Vec3{1, 2, 3},
 		Vec3{4, 5, 6},
-	})
+	)
 
 	if !correct.ApproxEqualThreshold(transpose, 1e-4) {
 		t.Errorf("Transpose not correct. Got: %v, expected: %v", transpose, correct)
@@ -225,19 +232,19 @@ func TestTransposeTall(t *testing.T) {
 }
 
 func TestTransposeWide(t *testing.T) {
-	m := Mat2x3FromCols([3]Vec2{
+	m := Mat2x3FromCols(
 		Vec2{1, 2},
 		Vec2{3, 4},
 		Vec2{5, 6},
-	})
+	)
 
 	transpose := m.Transpose()
 
-	correct := Mat3x2FromRows([3]Vec2{
+	correct := Mat3x2FromRows(
 		Vec2{1, 2},
 		Vec2{3, 4},
 		Vec2{5, 6},
-	})
+	)
 
 	if !correct.ApproxEqualThreshold(transpose, 1e-4) {
 		t.Errorf("Transpose not correct. Got: %v, expected: %v", transpose, correct)
@@ -245,19 +252,19 @@ func TestTransposeWide(t *testing.T) {
 }
 
 func TestTransposeSquare(t *testing.T) {
-	m := Mat3FromCols([3]Vec3{
+	m := Mat3FromCols(
 		Vec3{1, 2, 3},
 		Vec3{4, 5, 6},
 		Vec3{7, 8, 9},
-	})
+	)
 
 	transpose := m.Transpose()
 
-	correct := Mat3FromRows([3]Vec3{
+	correct := Mat3FromRows(
 		Vec3{1, 2, 3},
 		Vec3{4, 5, 6},
 		Vec3{7, 8, 9},
-	})
+	)
 
 	if !correct.ApproxEqualThreshold(transpose, 1e-4) {
 		t.Errorf("Transpose not correct. Got: %v, expected: %v", transpose, correct)
