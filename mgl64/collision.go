e50b8efbc53feaa142c306f756c11a1d8747fb08
Jsor <Jragonmiris@gmail.com>
2014-07-04 01:44:06 -0700

    Copied changes to double package

---
+++ mgl64/collision.go
@@ -0,0 +1,149 @@
+package mgl64
+
+// AABB stands for Axis-Aligned Bounding Box -- a box whose planes
+// are parallel with the 3 basis axes of your coordinate system.
+//
+// These are generally used for simple broad-phase collision tests.
+// We often use them for quick and dirty culling in graphics, but this could
+// be used as the foundation for the very basics of a physics engine.
+type AABB struct {
+	Min, Max Vec3
+}
+
+// Tests whether a point lies on or inside the AABB.
+func (aabb *AABB) IsPointInside(point Vec3) bool {
+	for i := 0; i < 3; i++ {
+		if aabb.Min[i] > point[i] || aabb.Max[i] < point[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Tests whether or not two AABBs collide -- "collision" is defined
+// here as they are touching OR one is completely engulfed by the other.
+func (aabb1 *AABB) Collides(aabb2 AABB) bool {
+	for i := 0; i < 3; i++ {
+		if aabb1.Min[i] > aabb2.Max[i] || aabb1.Max[i] < aabb2.Min[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Tests whether or not a triangle given by the points p1, p2, and p3 collides with
+// the AABB -- where "collision" is defined as they intersect at some point of one engulfs the
+// other. The points p1, p2, and p3 are not required to be in any specific order.
+func (aabb *AABB) CollidesTriangle(p1, p2, p3 Vec3) bool {
+	for i := 0; i < 3; i++ {
+		if Min(Min(p1[i], p2[i]), p3[i]) > aabb.Max[i] || Max(Max(p1[i], p2[i]), p3[i]) < aabb.Min[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+// Computes the inverse direction of a ray. This is equivalent
+// to the vector where each element is the reciprocal of the elements
+// of the input vector.
+func InverseDirection(dir Vec3) Vec3 {
+	return Vec3{1 / dir[0], 1 / dir[1], 1 / dir[2]}
+}
+
+// Computes whether a ray with the given origin and direction intersects an AABB. This also returns the
+// parametric description of the ray at the points it intersects the box (that is, the distance from the ray's origin),
+// where tmin is the entry point and tmax the exit point.
+//
+// This is a simple passthrough to the Precomputed version -- it computes the inverse direction, the sign
+// array, and unpacks the AABB into the bounds array.
+//
+// If the returned t values are negative, then the "intersection" is behind the ray. If the returned t values
+// have different signs, the ray originated from inside the box.
+func (aabb *AABB) IntersectsRay(rayOrigin, rayDirection Vec3) (intersects bool, tmin, tmax float64) {
+	invDir := InverseDirection(rayDirection)
+	return aabb.IntersectsRayInverseDir(rayOrigin, invDir)
+}
+
+// This is equivalent to aabb.IntersectsRay, except with a precomputed inverse direction. It will extract the AABB
+// into the bounds array, and infer the sign vector from the inverse direction. This is much better than IntersectsRay,
+// and you should probably use this if you don't want maintain references to ugly types like the sign or bounds arrays yourself.
+func (aabb *AABB) IntersectsRayInverseDir(rayOrigin, inverseRayDirection Vec3) (intersects bool, tmin, tmax float64) {
+	bounds := [2]Vec3{aabb.Min, aabb.Max}
+	sign := [3]int{}
+	for i := range sign {
+		if inverseRayDirection[i] < 0 {
+			sign[i] = 1
+		} else {
+			sign[i] = 0
+		}
+	}
+
+	return RayIntersectsAABBFromPrecomputedInfo(rayOrigin, inverseRayDirection, sign, bounds)
+}
+
+// This is equivalent to aabb.IntersectsRay, but has a precomputed sign vector, inverse direction, and uses a bounds array for the aabb's min and max.
+//
+// The bounds array is equivalent to [2]float32{aabb.Min, aabb.Max} and the sign is equivalent to whether each component inverseRayDirection is < 0
+// that is, if inverseRayDirection[i] < 0, then sign[i] = 1, otherwise sign[i] = 0.
+//
+// This returns whether there was an intersection. If so, the values of tmin and tmax will be parametric description of the ray where it intersects the box (roughly the distance at that point).
+//
+// You should probably use this rather than aabb.IntersectsRay, but the former is provided because it "looks" a lot cleaner. IntersectsRayInverseDir is
+// a pretty happy medium, but still slower.
+//
+// If the returned t values are negative, then the "intersection" is behind the ray. If the returned t values
+// have different signs, the ray originated from inside the box.
+func RayIntersectsAABBFromPrecomputedInfo(rayOrigin, inverseRayDirection Vec3, sign [3]int, bounds [2]Vec3) (intersects bool, tmin, tmax float64) {
+	// This is effectively equivalent to
+	/* if inverseRayDirection.X() < 0 {
+		tmin = (aabb.Min.X() - rayOrigin.X()) * inverseRayDirection.X()
+	} else {
+		tmin = (aabb.Max.X - rayOrigin.X()) * inverseRayDirection.X()
+	}
+	*/
+	// Effectively, these checks control for the direction of the ray, swapping max and min
+	// depending on the direction
+	tmin = (bounds[sign[0]].X() - rayOrigin.X()) * inverseRayDirection.X()
+	tmax = (bounds[1-sign[0]].X() - rayOrigin.X()) * inverseRayDirection.X()
+
+	tymin := (bounds[sign[1]].Y() - rayOrigin.Y()) * inverseRayDirection.Y()
+	tymax := (bounds[1-sign[1]].Y() - rayOrigin.Y()) * inverseRayDirection.Y()
+
+	if tmin > tymax || tymin > tmax {
+		return false, tmin, tmax
+	}
+
+	if tymin > tmin {
+		tmin = tymin
+	}
+	if tymax < tmax {
+		tmax = tymax
+	}
+
+	tzmin := (bounds[sign[2]].Z() - rayOrigin.Z()) * inverseRayDirection.Z()
+	tzmax := (bounds[1-sign[2]].Z() - rayOrigin.Z()) * inverseRayDirection.Z()
+	if tmin > tzmax || tzmin > tmax {
+		return false, tmin, tmax
+	}
+	if tzmin > tmin {
+		tmin = tzmin
+	}
+	if tzmax < tmax {
+		tmax = tzmax
+	}
+
+	return true, tmin, tmax
+}
+
+// Transforms the AABB with the given transformation matrix, this should correctly handle scaling.
+func (aabb *AABB) Transform(transformation Mat4) *AABB {
+	center, extent := aabb.Min.Add(aabb.Max).Mul(.5), aabb.Max.Sub(aabb.Min).Mul(.5)
+
+	newCenter := TransformCoordinate(center, transformation)
+	newExtent := TransformNormal(extent, transformation.Abs())
+
+	return &AABB{Min: newCenter.Sub(newExtent), Max: newCenter.Add(newExtent)}
+}
---
+++ mgl64/collision_test.go
@@ -0,0 +1,106 @@
+package mgl64
+
+import (
+	"testing"
+)
+
+func TestPointCollision(t *testing.T) {
+	var pointTests = [...]struct {
+		aabb  AABB
+		point Vec3
+
+		out bool
+	}{
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Vec3{.5, .5, .5}, true},
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Vec3{1, 1, 1.5}, false},
+	}
+
+	for _, test := range pointTests {
+		if test.aabb.IsPointInside(test.point) != test.out {
+			t.Errorf("Test of AABB %v and Point %v does not return expected result %v", test.aabb, test.point, test.out)
+		}
+	}
+}
+
+func TestAABBCollision(t *testing.T) {
+	var aabbTests = [...]struct {
+		aabb1 AABB
+		aabb2 AABB
+
+		out bool
+	}{
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, AABB{Min: Vec3{-.5, -.5, -.5}, Max: Vec3{.5, .5, .5}}, true},
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, AABB{Min: Vec3{-.5, -.5, -.5}, Max: Vec3{-.1, -.1, -.1}}, false},
+	}
+
+	for _, test := range aabbTests {
+		if test.aabb1.Collides(test.aabb2) != test.out {
+			t.Errorf("Test of AABB %v and AABB %v does not return expected result %v", test.aabb1, test.aabb2, test.out)
+		}
+	}
+}
+
+func TestTriangleCollision(t *testing.T) {
+	var triangleTests = [...]struct {
+		aabb       AABB
+		p1, p2, p3 Vec3
+
+		out bool
+	}{
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Vec3{.5, .5, .5}, Vec3{0, 0, 0}, Vec3{1, 1, 1}, true},
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Vec3{1.1, 1.1, 1.1}, Vec3{1.5, 1.5, 1.5}, Vec3{2.4, 3.7, 9.1}, false},
+	}
+
+	for _, test := range triangleTests {
+		if test.aabb.CollidesTriangle(test.p1, test.p2, test.p3) != test.out {
+			t.Errorf("Test of AABB %v and Triangle {%v,%v,%v} does not return expected result %v", test.aabb, test.p1, test.p2, test.p3, test.out)
+		}
+	}
+}
+
+func TestRayIntersection(t *testing.T) {
+	var rayTests = [...]struct {
+		aabb         AABB
+		rayFrom, dir Vec3
+
+		out          bool
+		correctedOut bool // If the intersection is behind the ray
+	}{
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Vec3{-1, .5, .5}, Vec3{1, 0, 0}, true, true},
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Vec3{-1.5, .5, .5}, Vec3{-1, 0, 0}, true, false},
+	}
+
+	for _, test := range rayTests {
+		ok, tmin, tmax := test.aabb.IntersectsRay(test.rayFrom, test.dir)
+		if ok != test.out {
+			t.Errorf("Test of AABB %v and ray starting at %v with direction %v does not return expected result %v.", test.aabb, test.rayFrom, test.dir, test.out)
+		}
+		if tmin < 0 && tmax < 0 {
+			ok = false
+		}
+		t.Logf("Got tmin, tmax %v; %v", tmin, tmax)
+		if ok != test.correctedOut {
+			t.Errorf("Test of AABB %v and ray starting at %v with direction %v does not return expected result %v after correcting for direction.", test.aabb, test.rayFrom, test.dir, test.correctedOut)
+		}
+	}
+}
+
+func TestAABBTransform(t *testing.T) {
+	var transformTests = [...]struct {
+		aabb      AABB
+		transform Mat4
+
+		out AABB
+	}{
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Translate3D(0, 0, .5), AABB{Min: Vec3{0, 0, .5}, Max: Vec3{1, 1, 1.5}}},
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Ident4(), AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}},
+		{AABB{Min: Vec3{0, 0, 0}, Max: Vec3{1, 1, 1}}, Translate3D(0, 0, .5).Mul4(Scale3D(2, 1, 1)), AABB{Min: Vec3{0, 0, .5}, Max: Vec3{2, 1, 1.5}}},
+	}
+
+	for _, test := range transformTests {
+		out := test.aabb.Transform(test.transform)
+		if !out.Min.ApproxEqualThreshold(test.out.Min, 1e-4) || !out.Max.ApproxEqualThreshold(test.out.Max, 1e-4) {
+			t.Errorf("AABB %v transformed by matrix %v results in AABB %v and not AABB %v", test.aabb, test.transform, out, test.out)
+		}
+	}
+}
