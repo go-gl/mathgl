3001711f05740d681344aa899d7ca2d591bae959
Jsor <Jragonmiris@gmail.com>
2014-07-04 00:02:52 -0700

    Finished AABB collision methods

--- mgl32/collision.go
+++ mgl32/collision.go
@@ -4,7 +4,7 @@ type AABB struct {
 	Min, Max Vec3
 }
 
-func PointInAABB(aabb AABB, point Vec3) bool {
+func (aabb *AABB) IsPointInside(point Vec3) bool {
 	for i := 0; i < 3; i++ {
 		if aabb.Min[i] > point[i] || aabb.Max[i] < point[i] {
 			return false
@@ -14,7 +14,7 @@ func PointInAABB(aabb AABB, point Vec3) bool {
 	return true
 }
 
-func AABBsCollide(aabb1 AABB, aabb2 AABB) bool {
+func (aabb1 *AABB) Collides(aabb2 AABB) bool {
 	for i := 0; i < 3; i++ {
 		if aabb1.Min[i] > aabb2.Max[i] || aabb1.Max[i] < aabb2.Min[i] {
 			return false
@@ -24,7 +24,7 @@ func AABBsCollide(aabb1 AABB, aabb2 AABB) bool {
 	return true
 }
 
-func TriangleCollidesAABB(aabb AABB, p1, p2, p3 Vec3) {
+func (aabb *AABB) CollidesTriangle(p1, p2, p3 Vec3) bool {
 	for i := 0; i < 3; i++ {
 		if Min(Min(p1[i], p2[i]), p3[i]) > aabb.Max[i] || Max(Max(p1[i], p2[i]), p3[i]) < aabb.Min[i] {
 			return false
@@ -34,11 +34,99 @@ func TriangleCollidesAABB(aabb AABB, p1, p2, p3 Vec3) {
 	return true
 }
 
-func InverseRayIntersectsAABB(rayOrigin, inverseRayDirection Vec3, raySign [3]uint, bounds [2]Vec3, lambdaMin, lambdaMax float32) (bool, float32) {
-	tmin := (bounds[raySign[0]].X() - rayOrigin.X()) * inverseRayDirection.X()
+// Computes the inverse direction of a ray. This is equivalent
+// to the vector where each element is the reciprocal of the elements
+// of the input vector.
+func InverseDirection(dir Vec3) Vec3 {
+	return Vec3{1 / dir[0], 1 / dir[1], 1 / dir[2]}
 }
 
-func TransformAABB(aabb AABB, transformation Mat3) AABB {
-	center, extent := aabb.Min.Add(aabb.Max).Mul(.5), aabb.Max.Sub(aabb.Min).Mul(.5)
+// Computes whether a ray with the given origin and direction intersects an AABB. This also returns the
+// parametric description of the ray at the point it intersects the box.
+//
+// This is a simple passthrough to the Precomputed version -- it computes the inverse direction, the sign
+// array, and unpacks the AABB into the bounds array.
+func (aabb *AABB) IntersectsRay(rayOrigin, rayDirection Vec3) (intersects bool, tmin, tmax float32) {
+	invDir := InverseDirection(rayDirection)
+	return rayIntersectsAABBInverseDir(rayOrigin, invDir, aabb)
+}
 
+// Here in case we want to export it. I thought 3 types of ray/direction tests were too much.
+func rayIntersectsAABBInverseDir(rayOrigin, inverseRayDirection Vec3, aabb AABB) (intersects bool, tmin, tmax float32) {
+	bounds := [2]Vec3{aabb.Min, aabb.Max}
+	sign := [3]int{}
+	for i := range sign {
+		if inverseRayDirection[i] < 0 {
+			sign[i] = 1
+		} else {
+			sign[i] = 0
+		}
+	}
+
+	return RayIntersectsAABBFromPrecomputedInfo(rayOrigin, inverseRayDirection, sign, bounds)
+}
+
+// This is equivalent to aabb.IntersectsRay, but has a precomputed sign vector and uses a bounds array.
+//
+// The bounds array is equivalent to [2]{aabb.Min, aabb.Max} and the sign is equivalent to whether each component inverseRayDirection is < 0
+// that is, if inverseRayDirection[i] < 0, then sign[i] = 1, otherwise sign[i] = 0.
+//
+// This returns whether there was an intersection. If so, the values of tmin and tmax will be parametric description of the ray where it intersects the box.
+func RayIntersectsAABBFromPrecomputedInfo(rayOrigin, inverseRayDirection Vec3, sign [3]int, bounds [2]Vec3) (intersects bool, tmin, tmax float32) {
+	// This is effectively equivalent to
+	/* if inverseRayDirection.X() < 0 {
+		tmin = (aabb.Min.X() - rayOrigin.X()) * inverseRayDirection.X()
+	} else {
+		tmin = (aabb.Max.X - rayOrigin.X()) * inverseRayDirection.X()
+	}
+	*/
+	// Effectively, these checks control for the direction of the ray, swapping max and min
+	// depending on the direction
+	tmin = (bounds[sign[0]].X() - rayOrigin.X()) * inverseRayDirection.X()
+	tmax = (bounds[1-sign[0]].X() - rayOrigin.X()) * inverseRayDirection.X()
+
+	tymin := (bounds[sign[1]].Y() - rayOrigin.X()) * inverseRayDirection.X()
+	tymax := (bounds[1-sign[1]].Y() - rayOrigin.X()) * inverseRayDirection.X()
+
+	if tmin > tymax || tymin > tmax {
+		return false, tmin, tmax
+	}
+
+	if tymin > tmin {
+		tmin = tymin
+	}
+	if tymax < tmax {
+		tmax = tymax
+	}
+
+	tzmin := (bounds[sign[2]].Z() - rayOrigin.Z()) * inverseRayDirection.Z()
+	tzmax := (bounds[1-sign[2]].Z() - rayOrigin.Z()) * inverseRayDirection.Z()
+	if tmin > tzmax || tzmin > tmax {
+		return false, tmin, tmax
+	}
+	if tzmin > tmin {
+		tmin = tzmin
+	}
+	if tzmax < tmax {
+		tmax = tzmax
+	}
+
+	return true, tmin, tmax
+}
+
+// Transforms the AABB with the given transformation matrix.
+func (aabb *AABB) Transform(transformation Mat4) *AABB {
+	min := Vec4{aabb.Min[0], aabb.Min[1], aabb.Min[2], 0}
+	max := Vec4{aabb.Max[0], aabb.Max[1], aabb.Max[2], 0}
+	center, extent := min.Add(max).Mul(.5), max.Sub(min).Mul(.5)
+
+	center[3] = 1 // only apply translation to the center, not the extent
+
+	newCenter := transformation.Mul4x1(center)
+	newCenter = newCenter.Mul(1 / newCenter[3]) // Project onto the w=1 plane
+	newExtent := transformation.Abs().Mul4x1(extent)
+
+	min = newCenter.Sub(newExtent)
+	max = newCenter.Add(newExtent)
+	return &AABB{Min: Vec3{min[0], min[1], min[2]}, Max: Vec3{max[0], max[1], max[2]}}
 }
