29499b1e4dae7413ca5afdc3ae0febee5ad43837
PucklaMotzer09 <jonaas.pucher000000@gmail.com>
2019-10-16 09:31:16 +0200

    Readd String methods to Vecs of mgl32

--- mgl32/vector.go
+++ mgl32/vector.go
@@ -9,6 +9,7 @@ package mgl32
 
 import (
 	"math"
+	"strconv"
 
 	"golang.org/x/image/math/f32"
 )
@@ -273,6 +274,14 @@ func (v1 Vec2) OuterProd4(v2 Vec4) Mat2x4 {
 	return Mat2x4{v1[0] * v2[0], v1[1] * v2[0], v1[0] * v2[1], v1[1] * v2[1], v1[0] * v2[2], v1[1] * v2[2], v1[0] * v2[3], v1[1] * v2[3]}
 }
 
+// String returns a string representation of Vec2
+func (v1 Vec2) String() string {
+	return "(" +
+		strconv.FormatFloat(float64(v1[0]), 'f', 3, 32) + ";" +
+		strconv.FormatFloat(float64(v1[1]), 'f', 3, 32) +
+		")"
+}
+
 // Project projects v1 onto the line from a to b
 func (v1 Vec2) Project(a, b Vec2) Vec2 {
 	var p Vec2
@@ -462,6 +471,15 @@ func (v1 Vec3) OuterProd3(v2 Vec3) Mat3 {
 // = Mat2x1*Mat1x3 = Mat2x3.
 func (v1 Vec3) OuterProd4(v2 Vec4) Mat3x4 {
 	return Mat3x4{v1[0] * v2[0], v1[1] * v2[0], v1[2] * v2[0], v1[0] * v2[1], v1[1] * v2[1], v1[2] * v2[1], v1[0] * v2[2], v1[1] * v2[2], v1[2] * v2[2], v1[0] * v2[3], v1[1] * v2[3], v1[2] * v2[3]}
+}
+
+// String returns a string representation of Vec3
+func (v1 Vec3) String() string {
+	return "(" +
+		strconv.FormatFloat(float64(v1[0]), 'f', 3, 32) + ";" +
+		strconv.FormatFloat(float64(v1[1]), 'f', 3, 32) + ";" +
+		strconv.FormatFloat(float64(v1[2]), 'f', 3, 32) +
+		")"
 }
 
 // Project projects v1 onto the line from a to b
@@ -661,4 +679,14 @@ func (v1 Vec4) OuterProd3(v2 Vec3) Mat4x3 {
 // = Mat2x1*Mat1x3 = Mat2x3.
 func (v1 Vec4) OuterProd4(v2 Vec4) Mat4 {
 	return Mat4{v1[0] * v2[0], v1[1] * v2[0], v1[2] * v2[0], v1[3] * v2[0], v1[0] * v2[1], v1[1] * v2[1], v1[2] * v2[1], v1[3] * v2[1], v1[0] * v2[2], v1[1] * v2[2], v1[2] * v2[2], v1[3] * v2[2], v1[0] * v2[3], v1[1] * v2[3], v1[2] * v2[3], v1[3] * v2[3]}
+}
+
+// String returns a string representation of Vec4
+func (v1 Vec4) String() string {
+	return "(" +
+		strconv.FormatFloat(float64(v1[0]), 'f', 3, 32) + ";" +
+		strconv.FormatFloat(float64(v1[1]), 'f', 3, 32) + ";" +
+		strconv.FormatFloat(float64(v1[2]), 'f', 3, 32) + ";" +
+		strconv.FormatFloat(float64(v1[3]), 'f', 3, 32) +
+		")"
 }
