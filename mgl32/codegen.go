2657cac1c4d0bc2203ee341a558a8d20e396c1ff
Egon Elbre <egonelbre@gmail.com>
2023-10-26 10:29:14 +0300

    code formatting

--- mgl32/codegen.go
+++ mgl32/codegen.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build ignore
 // +build ignore
 
 // codegen generates go code from templates. Intended to be
